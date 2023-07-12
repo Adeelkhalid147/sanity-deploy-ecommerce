a695330e03c1e0594faa5f7aadaaed843a6518a9
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-07-12 06:31:37 +0500

    new

--- src/components/views/subComponents/Cartstate.tsx
+++ src/components/views/subComponents/Cartstate.tsx
@@ -2,6 +2,7 @@
 import { CgShoppingCart } from 'react-icons/cg'
 import { useContext, useEffect, useState } from 'react'
 import { cartContext } from '@/global/Context'
+import Link from 'next/link'
 
 const Cartstate = () => {
 const [quantity,setQuantity] = useState(0)
@@ -13,12 +14,12 @@ useEffect(()=>{
 },[])
 if (isBrowser()){
   return (
-  <div className='flex-shrink bg-[#F1F1F1] relative rounded-full h-10 w-10 flex justify-center items-center'>
+  <Link href={"/cart"} className='flex-shrink bg-[#F1F1F1] relative rounded-full h-10 w-10 flex justify-center items-center'>
    <div className='absolute bg-[#e9a7aa] h-3 w-3 text-xs rounded-full flex justify-center items-center top-1 right-1'>
               {quantity}
    </div>
           <CgShoppingCart size={25}/>
-   </div>
+   </Link>
    )
 } else{
   return <p>Loading</p>
