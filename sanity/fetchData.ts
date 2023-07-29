4deff1207d62fb0b1ae376c1f355a290cdcfa4b2
AdeelKhalid147 <adeelkhalid147@gmail.com>
2023-07-29 20:48:41 +0500

    changes

--- sanity/fetchData.ts
+++
@@ -1,6 +0,0 @@
-import { client } from "./lib/client";
-
-export async function getProducts() {
-    const res = await client.fetch(`*[_type == "products"]`);
-    return res;
-  }
--- src/app/catalog/[slug]/page.tsx
+++ src/app/catalog/[slug]/page.tsx
@@ -1,47 +1,47 @@
-// import BASE_PATH_FORAPI from '@/components/shared/Wrapper/BasePath'
-// import ContextWrapper from '../../../global/Context'
-// import { responseType, oneProductType } from '@/components/utils/ProductsDataArrayAndType'
-// import ProductDetail from "@/components/views/productDetail"
-// import { FC } from "react"
-// import { Metadata } from 'next'
-// import {getProducts} from '../../../../sanity/fetchData'
+import BASE_PATH_FORAPI from '@/components/shared/Wrapper/BasePath'
+import ContextWrapper from '../../../global/Context'
+import { responseType, oneProductType } from '@/components/utils/ProductsDataArrayAndType'
+import ProductDetail from "@/components/views/productDetail"
+import { FC } from "react"
+import { Metadata } from 'next'
 
-// // browser k top pe jo product open ho us ka name show krne k liye generateMetadata ye function
-// export async function generateMetadata({ params }:{params: { slug:string }}) {
-//   // read route params
-//   const slug = params.slug
 
-//   // fetch data
-//   const product = await fetch(`https://ezi31s7j.api.sanity.io/v2023-06-30/data/query/production?query=*[_type == 'products']`).then((res:any) => res.json())
-//  const titleToSet:oneProductType = product.result.find((item:oneProductType) => item.slug.current === slug)
-//   return {
-//    title: titleToSet.productName,
-//    description: titleToSet.description,
-//   }
-// }
+// browser k top pe jo product open ho us ka name show krne k liye generateMetadata ye function
+export async function generateMetadata({ params }:{params: { slug:string }}) {
+  // read route params
+  const slug = params.slug
 
-// // fetch data of product using slug
-// async function fetchPreviewData(slug:string) {
-//   let res = await fetch(`https://ezi31s7j.api.sanity.io/v2023-06-30/data/query/production?query=*%5B_type+%3D%3D+%22products%22+%26%26+slug.current+%3D%3D+%22${slug}%22%5D`)
-//   return res.json()
-// }
+  // fetch data
+  const product = await fetch(`https://ezi31s7j.api.sanity.io/v2023-06-30/data/query/production?query=*[_type == 'products']`).then((res:any) => res.json())
+ const titleToSet:oneProductType = product.result.find((item:oneProductType) => item.slug.current === slug)
+  return {
+   title: titleToSet.productName,
+   description: titleToSet.description,
+  }
+}
 
-// /*
-// multiple page bnane k liye getStaticProps (genrateStaticParams) ye function bnya h or return mai array
-// lga k object mai jo slug likha h wo page ka name h catalog\[slug].  {slug: "american"}
-// jitni dfa ye likhen gy uthne page bnyn g. jha jha b slug likha h wo as liye k folder
-// mai page ka name slug h
-//  */
-// // export async function generateStaticParams(){
-// //   let res = await fetch(`https://ezi31s7j.api.sanity.io/v2023-06-30/data/query/production?query=*[_type == 'products']`,{
-// //     next: {
-// //       revalidate:60
-// //     }
-// //   }).then((res:any) => res.json())
-// // // console.log("Res:", res)
-// // return res.result.map((item:oneProductType) => { slug:item.slug})
+// fetch data of product using slug
+async function fetchPreviewData(slug:string) {
+  let res = await fetch(`https://ezi31s7j.api.sanity.io/v2023-06-30/data/query/production?query=*%5B_type+%3D%3D+%22products%22+%26%26+slug.current+%3D%3D+%22${slug}%22%5D`)
+  return res.json()
+}
 
-// // }
+/*
+multiple page bnane k liye getStaticProps (genrateStaticParams) ye function bnya h or return mai array
+lga k object mai jo slug likha h wo page ka name h catalog\[slug].  {slug: "american"}
+jitni dfa ye likhen gy uthne page bnyn g. jha jha b slug likha h wo as liye k folder
+mai page ka name slug h
+ */
+export async function generateStaticParams(){
+  let res = await fetch(`https://ezi31s7j.api.sanity.io/v2023-06-30/data/query/production?query=*[_type == 'products']`,{
+    next: {
+      revalidate:60
+    }
+  }).then((res:any) => res.json())
+// console.log("Res:", res)
+return res.result.map((item:oneProductType) => { slug:item.slug})
+
+}
 
 // export async function generateStaticParams() {
 //   // const data = await fetch(`https://ezi31s7j.api.sanity.io/v2023-06-30/data/query/production?query=*[_type == 'products']`).then((res:any) => res)
@@ -51,77 +51,14 @@
 //   }));
 // }
 
-// const Catalog = async ({ params }:{params:{slug:string} }) => {
-//   let data:responseType = await fetchPreviewData(params.slug)
-//   return (
-
-//       <ContextWrapper>
-//       <ProductDetail item={data.result[0]}/>
-//       </ContextWrapper>
-//   )
-// }
-
-// export default Catalog
-
-import {
-  responseType,
-  oneProductType,
-} from "@/components/utils/ProductsDataArrayAndType";
-import ProductDetail from "@/components/views/productDetail";
-import ContextWrapper from "@/global/Context";
-
-
-// metadata genrator
-export async function generateMetadata({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const slug = params.slug;
-
-  const product = await fetch(
-    `https://ezi31s7j.api.sanity.io/v2023-05-26/data/query/production?query=*[_type == 'products']`
-  ).then((res: any) => res.json());
-  const titleToSet: oneProductType = product.result.find(
-    (item: oneProductType) => item.slug.current == slug
-  );
-
-  return {
-    title: titleToSet.productName,
-    description: titleToSet.description,
-  };
-}
-
-// fetch particular data of product using slug
-async function fetchPreviewData(slug: string) {
-  let res = await fetch(
-    `https://ezi31s7j.api.sanity.io/v2023-05-26/data/query/production?query=*%5B_type%20%3D%3D%20%22products%22%20%26%26%20slug.current%3D%3D%20%22${slug}%22%5D`
-  );
-  return res.json();
-}
-
-// will make static pages of every product
-export async function generateStaticParams() {
-  let res = await fetch(
-    `https://ezi31s7j.api.sanity.io/v2023-05-26/data/query/production?query=*[_type == 'products']`,
-    {
-      next: {
-        revalidate: 60,
-      },
-    }
-  ).then((res: any) => res.json());
-  return res.result.map((item: oneProductType) => {
-    slug: item.slug;
-  });
-}
-
-const Catalog = async ({ params }: { params: { slug: string } }) => {
-  let data: responseType = await fetchPreviewData(params.slug);
+const Catalog = async ({ params }:{params:{slug:string} }) => {
+  let data:responseType = await fetchPreviewData(params.slug)
   return (
-    <ContextWrapper>
-      <ProductDetail item={data.result[0]} />
-    </ContextWrapper>
-  );
-};
 
-export default Catalog;
+      <ContextWrapper>
+      <ProductDetail item={data.result[0]}/>
+      </ContextWrapper>
+  )
+}
+
+export default Catalog
